bc3b5f545cf7ebae83982731a26f8bd9e6727743
nishant <nishant.singh87@gmail.com>
2016-07-30 19:50:56 +0530

    create maven project with wiremockand junit

---
+++ src/test/java/TestWiredex.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertEquals;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class TestWiredex {
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8089); // No-args constructor defaults to port 8080
+
+    @Test
+    public void evaluatesExpression() {
+        assertEquals(6, 7);
+    }
+
+    @Test
+    public void exampleTest() {
+        stubFor(get(urlEqualTo("/my/resource"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>Some content</response>")));
+
+//        Result result = myHttpServiceCallingObject.doSomething();
+//
+//        assertTrue(result.wasSuccessFul());
+
+        verify(postRequestedFor(urlMatching("/my/resource/[a-z0-9]+"))
+                .withRequestBody(matching(".*<message>1234</message>.*"))
+                .withHeader("Content-Type", notMatching("application/json")));
+    }
+}
